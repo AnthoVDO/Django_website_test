289f0f7b7e3211d5ddb466137a8ae6022304061b
Antho VDO Novu <43015083+AnthoVDO@users.noreply.github.com>
2022-12-29 13:06:48 +0100

    continuing the course

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -6,6 +7,20 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
     published = models.BooleanField(default=False)
-    date = models.DateField(blank=True)
+    date = models.DateField(blank=True, null=True)
     content = models.TextField()
-    description = models.TextField()
+    description = models.TextField()  
+    
+    # Used to avoid to put the () in the end example: BlogPost.published_string instead of BlogPost.published_string()
+    @property # It says that it's a property instead of a method  
+    def published_string(self):
+        if self.published:
+            return "The article is published"  
+        return "The article isn't published" 
+
+    def save(self, *args, **kwargs): # Overload the save method
+
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super().save(*args, **kwargs)
